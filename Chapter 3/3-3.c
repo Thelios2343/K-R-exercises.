6fcc95aace51a07e0a540c434e6a33a4afaaea3f
Thelios <jumaun47@gmail.com>
2026-06-03 14:28:45 -0500

    Add files via upload

--- Chapter 3/3-3.c
+++ Chapter 3/3-3.c
@@ -6,18 +6,15 @@ void expand (char s1[], char s2[]);
 int same_type (char a, char b) {
        
     /* minúsculas */
-    if (a >= 'a' && a <= 'z' &&
-        b >= 'a' && b <= 'z'){
+    if (a >= 'a' && a <= 'z' && b >= 'a' && b <= 'z'){
         return 1;
     }
     /* mayúsculas */
-    if (a >= 'A' && a <= 'Z' &&
-        b >= 'A' && b <= 'Z') {
+    if (a >= 'A' && a <= 'Z' && b >= 'A' && b <= 'Z') {
         return 1;
     }
     /* dígitos */
-    if (a >= '0' && a <= '9' &&
-        b >= '0' && b <= '9') {
+    if (a >= '0' && a <= '9' && b >= '0' && b <= '9') {
         return 1;
     }
 
@@ -32,18 +29,9 @@ void expand (char s1[], char s2[]) {
 
     while (s1[i] != '\0') {
 
-        if (
-            s1[i] == '-' &&
-            i > 0 &&
-            s1[i + 1] != '\0' &&
-            same_type(s1[i - 1], s1[i + 1]) &&
-            s1[i - 1] <= s1[i + 1]) {
+        if (s1[i] == '-' && i > 0 && s1[i + 1] != '\0' && same_type(s1[i - 1], s1[i + 1]) && s1[i - 1] <= s1[i + 1]) {
 
-
-            for (c = s1[i - 1] + 1;
-                 c < s1[i + 1];
-                 c++) {
-
+            for (c = s1[i - 1] + 1;c < s1[i + 1];c++) {
                 s2[j++] = c;
             }
         } 
@@ -58,9 +46,6 @@ void expand (char s1[], char s2[]) {
     s2[j] = '\0';
 }
 
-
-
-
 int main () {
     
     char s1[] = "a-z";
@@ -72,3 +57,4 @@ int main () {
 
     return 0;
 }
+
