39b7531149f9f843d1068b44d2037ff49691ea31
Thelios <jumaun47@gmail.com>
2026-07-05 00:46:04 -0500

    Add files via upload

---
+++ Chapter 5/5-5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+void str_cpy(char *s, char *t, int n);
+int str_len(char *s);
+
+int main () {
+    
+    char s[100];
+    char t[100] = "hola";
+
+    str_cpy(s, t, 3);
+
+    printf("%s", s);
+
+}
+
+int str_len (char *s) {
+    int n = 0;
+
+    while (*s != '\0') {
+        s++;
+        n++;
+    }
+
+    return n;
+}
+
+void str_cpy(char *s, char *t, int n) {
+   int i;
+  
+
+   for (i = 0; i < n; i++) { 
+        *s = *t;
+         s++;
+         t++;
+   }
+   *s = '\0';
+
+}
