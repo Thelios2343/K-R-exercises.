d68def443f9d62d6e0616cd253a69c425c8ee8e1
Thelios <jumaun47@gmail.com>
2026-07-04 23:22:17 -0500

    Add files via upload

---
+++ Chapter 5/5-4.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int str_len(char *s);
+int strend(char *s, char *t);
+
+int main () {
+    
+    char s[100] = "Programa";
+    char t[100] = "ma";
+
+   printf("%d\n", strend(s, t) );
+}
+
+int str_len(char *s) {
+    int n = 0;
+    while (*s != '\0') {
+        s++;
+        n++;
+    }
+    return n;
+}
+
+int strend(char *s, char *t) {
+   
+    if (str_len(t) > str_len(s)) {
+        return 0;
+    }
+
+    s += str_len(s) - str_len(t);
+    
+    while (*t != '\0') {
+        
+
+        if (*s != *t) {
+            return 0;
+        }
+
+        s++;
+        t++;
+    }
+
+    return 1;
+}
